99d969fe3d31aadf6792dc4a2142ebe35a9f8b1e
Harry Pidcock <haza55@gmail.com>
2020-05-30 16:46:03 +1000

    Fix cache clearing.

--- cache/storer/storer.go
+++ cache/storer/storer.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"fmt"
 	gobuild "go/build"
+	"io/ioutil"
 	"os"
 	"path"
-	"path/filepath"
+	"strings"
 
 	"github.com/hpidcock/gophertest/builder"
 	"github.com/hpidcock/gophertest/util"
@@ -23,36 +24,7 @@ type Storer struct {
 	CacheDir string
 }
 
-func (s *Storer) Cleanup(ctx context.Context, node *dag.Node) error {
-	if node.ImportPath == "main" {
-		return nil
-	}
-
-	rebuilt := false
-	for _, meta := range node.Meta {
-		switch m := meta.(type) {
-		case *builder.BuildMeta:
-			rebuilt = m.Rebuilt
-		}
-	}
-	if !rebuilt {
-		return nil
-	}
-
-	if node.Shlib == "" {
-		return fmt.Errorf("missing shlib")
-	}
-
-	cacheDir := util.PackageCacheDir(s.CacheDir, node.ImportPath)
-	err := os.RemoveAll(cacheDir)
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
-	return nil
-}
-
-func (s *Storer) Update(ctx context.Context, node *dag.Node) error {
+func (s *Storer) Visit(ctx context.Context, node *dag.Node) (errOut error) {
 	if node.ImportPath == "main" {
 		return nil
 	}
@@ -79,11 +51,44 @@ func (s *Storer) Update(ctx context.Context, node *dag.Node) error {
 	}
 
 	manifestFilepath := path.Join(cacheDir, fmt.Sprintf("%s.manifest", node.Name))
+	if _, err := os.Stat(manifestFilepath); os.IsNotExist(err) {
+		// Do nothing
+	} else if err != nil {
+		return errors.WithStack(err)
+	} else {
+		// Remove old files from previous cache.
+		manifestBytes, err := ioutil.ReadFile(manifestFilepath)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		manifest := strings.Split(string(manifestBytes), "\n")
+		for _, f := range manifest {
+			filepath := path.Join(cacheDir, f)
+			err := os.Remove(filepath)
+			if os.IsNotExist(err) {
+				continue
+			} else if err != nil {
+				return errors.WithStack(err)
+			}
+		}
+	}
+
 	manifestFile, err := os.Create(manifestFilepath)
 	if err != nil {
 		return errors.WithStack(err)
 	}
-	defer manifestFile.Close()
+	defer func() {
+		err := manifestFile.Close()
+		if err != nil {
+			err = errors.WithStack(err)
+			if errOut != nil {
+				fmt.Println(err.Error())
+			} else {
+				errOut = err
+			}
+			return
+		}
+	}()
 
 	objFilename := fmt.Sprintf("%s.obj", node.Name)
 	_, err = fmt.Fprintln(manifestFile, objFilename)
@@ -96,16 +101,6 @@ func (s *Storer) Update(ctx context.Context, node *dag.Node) error {
 		return errors.WithStack(err)
 	}
 
-	existingGoFiles, err := filepath.Glob(path.Join(cacheDir, "*.go"))
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	for _, v := range existingGoFiles {
-		err := os.Remove(v)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-	}
 	for _, goFile := range node.GoFiles {
 		if goFile.Dir == node.SourceDir {
 			continue
@@ -123,16 +118,6 @@ func (s *Storer) Update(ctx context.Context, node *dag.Node) error {
 		}
 	}
 
-	existingSFiles, err := filepath.Glob(path.Join(cacheDir, "*.s"))
-	if err != nil {
-		return errors.WithStack(err)
-	}
-	for _, v := range existingSFiles {
-		err := os.Remove(v)
-		if err != nil {
-			return errors.WithStack(err)
-		}
-	}
 	for _, sFile := range node.SFiles {
 		if sFile.Dir == node.SourceDir {
 			continue
